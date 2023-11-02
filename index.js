90b89d2496adc8c1c30be75fd901adb549dddb15
Mesure73L <115181664+Mesure73L@users.noreply.github.com>
2023-11-02 11:55:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const topnav = document.getElementsByClassName("topnav");
 function ToggleDarkMode() {
     let element = document.body;
     element.classList.toggle("dark-mode");
-    topnav.classList.toggle("topnav-dark");
-    topnav.classList.toggle("topnav-light");
+    topnav.classList.toggle("topnavDark");
+    topnav.classList.toggle("topnavLight");
 
    
 
   }
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+/*if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     ToggleDarkMode();
 }
+*/
